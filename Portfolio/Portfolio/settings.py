10f547e05bf758c84b4d3d4394432d664ba21dca
generalShark404 <alaminhaidaraliyu@gmail.com>
2024-07-07 20:02:03 +0000

    Changed the install app name from portfolio_app_1 to Portfolio.portfolio_app_a

--- Portfolio/Portfolio/settings.py
+++ Portfolio/Portfolio/settings.py
@@ -41,7 +41,7 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
-    'portfolio_app_1',
+    'Portfolio.portfolio_app_1',
 ]
 
 MIDDLEWARE = [
